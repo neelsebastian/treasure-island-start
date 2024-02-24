fe18133a38112ad9823598c4fc016a781b98e4ca
Neel Sebastian <68556380+neelsebastian@users.noreply.github.com>
2024-02-24 09:16:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("You are at a crossroad. Where do you want to go?")
+direction = input("Type 'left' or 'right'.\n")
+if direction  == "left":
+  print("You have reached a lake. There is an island in the middle of the lake.")
+  choice = input("Type 'wait' to wait for a boat. Type 'swim' to cross the lake.\n")
+  if choice == "wait":
+    print("You have reached the Island through boat. There is a cave with 3 doors.")
+    door = input("Which door do you want to open? Type 'red', 'yellow' or 'blue'.\n")
+    if door == "red" or door == "blue":
+      print("You have entered into the cave of monsters. Game Over.")
+    else:
+      print("You have found the tressure. You win!")
+  else:
+    print("You have eaten by the sharks. Game Over.")
+else:
+  print("You have fallen into a hole. Game Over.")
